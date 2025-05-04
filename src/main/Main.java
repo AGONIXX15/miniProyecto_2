b57ebee0308f3b583c681b1e80a846fd8a7e1519
simon4585 <simon.tarazona@correounivalle.edu.co>
2025-05-03 23:00:02 -0500

    Ventana de inicio completa

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,13 +4,15 @@ import models.pokemon.Pokemon;
 import models.pokemon.utils.Attack;
 import models.pokemon.utils.TypeDamage;
 import models.pokemon.utils.TypePokemon;
+import view.MainFrame;
 
 
 
-public class
-Main {
+import javax.swing.SwingUtilities;
+
+public class Main {
     public static void main(String[] args) {
-
+        MainFrame mainFrame = new MainFrame();
     }
 }
 
