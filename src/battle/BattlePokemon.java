48a90d160e8ebcc9649083ed7ad506a7736ff049
Sebastian Rubio <rubio.sebastian@correounivalle.edu.co>
2025-05-04 19:09:31 -0500

    BattlePokemon and trainer modified

--- src/battle/BattlePokemon.java
+++ src/battle/BattlePokemon.java
@@ -3,65 +3,40 @@ package battle;
 import models.Trainer;
 import models.pokemon.Pokemon;
 import models.pokemon.utils.Attack;
+import view.battle.BattlePokemonGUI;
 //import view.PokemonMenu;
 
 import java.util.Scanner;
 
 public class BattlePokemon {
 
-    Pokemon pokemon1, pokemon2;
-    Trainer player1, player2;
-    public BattlePokemon(Pokemon pokemon1, Pokemon pokemon2, Trainer player1, Trainer player2) {
-        this.pokemon1 = pokemon1;
-        this.pokemon2 = pokemon2;
-        this.player1 = player1;
-        this.player2 = player2;
+    public BattlePokemon() {
     }
 
 
 
-    public Attack selectAttack(Pokemon pokemon) {
-        Scanner sc = new Scanner(System.in);
-        boolean condition = false;
-        int index;
-        do {
-           // PokemonMenu.showPokemonAttacks(pokemon);
-            index = Integer.parseInt(sc.nextLine())-1;
-            condition = !(index >= 0 && index < pokemon.getAttacks().length);
-            if(condition){
-                System.out.println("⚠️por favor ingresa uno de los ataques disponibles!");
-            }
-        }while(condition);
-        return pokemon.getAttacks()[index];
-    }
 
-    public void combat(){
-        // comienza el que menos vida actual tiene
-        int turn = (pokemon1.getHealth() >= pokemon2.getHealth()) ? 1 : 0;
-        while(pokemon1.isAlive() && pokemon2.isAlive()){
-            if (turn % 2 == 0) {
-                System.out.printf("%s" + " Escoge algun ataque!", player1.getNameTrainer());
-                Attack attack = selectAttack(pokemon1);
-                pokemon1.makeDamage(pokemon2, attack);
-            } else {
-                System.out.printf("%s" + " Escoge algun ataque!", player2.getNameTrainer());
-                Attack attack = selectAttack(pokemon2);
-                pokemon2.makeDamage(pokemon1, attack);
-            }
-            // se autoincrementa para poder intercalar los turnos
-            turn++;
+    public static boolean findOut(Pokemon pokemon1, Pokemon pokemon2){
+        if (pokemon1.isAlive() && pokemon2.isAlive()){
+            return false;
         }
 
+        boolean result = false;
         if(pokemon1.isAlive()){
-            System.out.printf("%s Gano la batalla pokemonn!\n", pokemon1.getName());
+            BattlePokemonGUI.getInstance().putMessage(String.format("%s Gano la batalla pokemonn!\n", pokemon1.getName()));
+            result = true;
         } else {
-            System.out.printf("%s Perdio la batalla pokemon!\n", pokemon1.getName());
+            BattlePokemonGUI.getInstance().putMessage(String.format("%s Perdio la batalla pokemon!\n", pokemon1.getName()));
+            result = true;
         }
 
         if(pokemon2.isAlive()){
-            System.out.printf("%s Gano la batalla pokemonn!\n", pokemon2.getName());
+            BattlePokemonGUI.getInstance().putMessage(String.format("%s Gano la batalla pokemonn!\n", pokemon2.getName()));
+            result = true;
         } else  {
-            System.out.printf("%s Perdio la batalla pokemon!\n", pokemon2.getName());
+            BattlePokemonGUI.getInstance().putMessage(String.format("%s Perdio la batalla pokemon!\n", pokemon2.getName()));
+            result = true;
         }
+        return result;
     }
 }
--- src/battle/BattleTrainer.java
+++ src/battle/BattleTrainer.java
@@ -2,22 +2,18 @@ package battle;
 
 import models.Trainer;
 import models.pokemon.Pokemon;
+import view.battle.BattlePokemonGUI;
 //import view.PokemonMenu;
 
 import java.util.Scanner;
 
 public class BattleTrainer {
 
-    private Trainer player1, player2;
-
-    public BattleTrainer(Trainer player1, Trainer player2) {
-        this.player1 = player1;
-        this.player2 = player2;
-    }
 
 
 
-    public boolean trainerHasLost(Trainer trainer) {
+
+    public static boolean trainerHasLost(Trainer trainer) {
         for(Pokemon pokemon: trainer.team){
             if(pokemon.isAlive()){
                 return false;
@@ -26,50 +22,20 @@ public class BattleTrainer {
         return true;
     }
 
-    public Pokemon choosePokemon(Trainer trainer) {
-        Scanner sc = new Scanner(System.in);
-        boolean condition;
-        int index;
-        do {
-           // PokemonMenu.showAllPokemonsAlive(trainer.team);
-            System.out.println( trainer.getNameTrainer() + " Escoje un pokemon de tu equipo para comenzar a luchar: ");
-            //PokemonMenu.showAllPokemonsAlive(trainer.team);
-            System.out.println(trainer.getNameTrainer() + " Escoje un pokemon de tu equipo para comenzar a luchar: ");
-            index = Integer.parseInt(sc.nextLine())-1;
-            condition = !(index >= 0 && index < trainer.team.length && trainer.team[index].isAlive());
-            if(!trainer.team[index].isAlive()){
-                System.out.println("escoje un pokemon que este vivo");
-            }
-        }while(condition);
-        Pokemon pokemon = trainer.team[index];
 
-        return pokemon;
-    }
-
-    public void combat() {
-
-        while (!trainerHasLost(player1) && !trainerHasLost(player2)) {
-            Pokemon pokemon1 = choosePokemon(player1);
-            Pokemon pokemon2 = choosePokemon(player2);
-            System.out.println("A LUCHAAAAAR!!");
-
-
-            BattlePokemon battlePokemon = new BattlePokemon(pokemon1, pokemon2, player1, player2);
-
-            battlePokemon.combat();
-
+    public static boolean combat(Trainer trainer1, Trainer trainer2) {
+        if(!BattleTrainer.trainerHasLost(trainer1) && !BattleTrainer.trainerHasLost(trainer2)) {
+            return false;
         }
 
-        if (trainerHasLost(player1)){
-            System.out.printf("%s Perdio la batalla pokemon!\n", player1.getNameTrainer());
+        if (BattleTrainer.trainerHasLost(trainer1)){
+            BattlePokemonGUI.getInstance().putMessage(String.format("%s Perdio la batalla pokemon!\n", trainer1.getNameTrainer()));
+            BattlePokemonGUI.getInstance().putMessage(String.format("%s gano la batalla pokemon!\n", trainer2.getNameTrainer()));
+            return true;
         } else {
-            System.out.printf("%s Gano la batalla pokemonn!", player1.getNameTrainer());
-        }
-
-        if (trainerHasLost(player2)){
-            System.out.printf("%s Perdio la batalla pokemon!\n", player2.getNameTrainer());
-        } else {
-            System.out.printf("%s Gano la batalla pokemonn!", player2.getNameTrainer());
+            BattlePokemonGUI.getInstance().putMessage(String.format("%s Gano la batalla pokemonn!", trainer1.getNameTrainer()));
+            BattlePokemonGUI.getInstance().putMessage(String.format("%s Perdio la batalla pokemonn!", trainer2.getNameTrainer()));
+            return true;
         }
     }
 }
